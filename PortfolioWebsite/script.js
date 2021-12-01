82754e8bbb7072262951dfb82c1a6ac7f51a2c5b
Achatin <37224828+Achatin@users.noreply.github.com>
2021-12-01 21:32:39 +0100

    Website Test

---
+++ PortfolioWebsite/script.js
@@ -0,0 +1,118 @@
+function dotScroll() {
+    let element = document.getElementById("container");
+    let y = element.scrollTop;
+
+    let notice = document.getElementById("notice");
+    if(y > 100) {
+        notice.style.opacity = 0;
+        notice.style.transition = "opacity 0.5s";
+    }
+}
+
+
+function carouselRpi() {
+    
+    var myIndex = 0;
+    var interval;
+    
+    function startSlide(){
+
+        var i;
+        var x = document.getElementsByClassName("hidden Rpi");
+
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+            myIndex++;
+            if (myIndex > x.length) {myIndex = 1}    
+            x[myIndex-1].style.display = "block";
+            interval = setTimeout(startSlide, 3000); // Change image every 3 seconds
+
+    }
+
+    
+    function stopSlide(){
+
+        clearTimeout(interval);
+
+        document.getElementById("slideRpi1").style.display = "none";
+        document.getElementById("slideRpi2").style.display = "none";
+        document.getElementById("slideRpi3").style.display = "none";
+
+    }
+
+    document.getElementById("previewRpi").addEventListener('mouseenter', startSlide);
+    document.getElementById("previewRpi").addEventListener('mouseleave', stopSlide);
+}
+
+
+function carouselWD() {
+    
+    var myIndex = 0;
+    var interval;
+    
+    function startSlide(){
+
+        var i;
+        var x = document.getElementsByClassName("hidden WD");
+
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+            myIndex++;
+            if (myIndex > x.length) {myIndex = 1}    
+            x[myIndex-1].style.display = "block";
+            interval = setTimeout(startSlide, 3000); // Change image every 3 seconds
+
+    }
+
+    
+function stopSlide(){
+
+        clearTimeout(interval);
+
+        document.getElementById("slideWD1").style.display = "none";
+        document.getElementById("slideWD2").style.display = "none";
+        document.getElementById("slideWD3").style.display = "none";
+
+    }
+
+    document.getElementById("previewWD").addEventListener('mouseenter', startSlide);
+    document.getElementById("previewWD").addEventListener('mouseleave', stopSlide);
+}
+
+
+function carouselCoding() {
+    
+    var myIndex = 0;
+    var interval;
+    
+    function startSlide(){
+
+        var i;
+        var x = document.getElementsByClassName("hidden Coding");
+
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+            myIndex++;
+            if (myIndex > x.length) {myIndex = 1}    
+            x[myIndex-1].style.display = "block";
+            interval = setTimeout(startSlide, 3000); // Change image every 3 seconds
+
+    }
+
+    
+function stopSlide(){
+
+        clearTimeout(interval);
+
+        document.getElementById("slideCoding1").style.display = "none";
+        document.getElementById("slideCoding2").style.display = "none";
+        document.getElementById("slideCoding3").style.display = "none";
+
+    }
+
+    document.getElementById("previewCoding").addEventListener('mouseenter', startSlide);
+    document.getElementById("previewCoding").addEventListener('mouseleave', stopSlide);
+}
---
+++ PortfolioWebsite/tailwind.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        '6vw': '6.25vw',
+        '6.4vw': '6.4vw',
+        '6.5vh': '6.5vh',
+        '7.5vw': '7.5vw',
+        '8vw': '8vw',
+        '10vh': '10vh',
+        '15vw': '15vw',
+        '20vh': '20vh',
+        '25vh': '25vh',
+        '26.5vh': '26.5vh',
+        '30vh': '30vh',
+        '35vh': '35vh',
+        '50vh': '50vh',
+        '100vh': '100vh',
+      },
+    },
+
+    fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'tiny': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+        '8vh': '8vh',
+        '15vh': '15vh',
+    }
+
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
