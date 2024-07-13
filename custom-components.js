49857c54a72b81339ccd4de9f01d9519c0661932
sn489yw <simon.nigut@student.tuke.sk>
2024-07-13 09:40:40 +0200

    portfolio update

--- custom-components.js
+++ custom-components.js
@@ -29,16 +29,25 @@ class TextIconBubbleElement extends HTMLElement {
 
 class ImageShowcaseElement extends HTMLElement {
   connectedCallback() {
+    this.img1 = this.getAttribute("img1");
+    this.img2 = this.getAttribute("img2");
+    this.img3 = this.getAttribute("img3");
+    this.img4 = this.getAttribute("img4");
+    this.img5 = this.getAttribute("img5");
+    this.img6 = this.getAttribute("img6");
+    this.img7 = this.getAttribute("img7");
+    this.img8 = this.getAttribute("img8");
+
     this.innerHTML = `
         <div class="image-showcase group relative aspect-[7/5] w-64">
-          <div class="absolute h-1/2 w-1/2 translate-x-1/2 translate-y-1/2 rounded-lg bg-gray-100 transition-transform delay-75 duration-300 ease-in-out group-hover:-translate-y-[65%] group-hover:translate-x-[200%] group-hover:-rotate-[15deg]"></div>
-          <div class="absolute h-full w-full -translate-x-4 -rotate-1 rounded-lg bg-gray-900 transition-transform delay-75 duration-300 ease-in-out group-hover:-translate-x-[75%] group-hover:translate-y-[16%] group-hover:-rotate-[24deg]"></div>
-          <div class="absolute h-1/2 w-1/2 translate-x-1/2 translate-y-1/2 rounded-lg bg-gray-200 transition-transform delay-75 duration-300 ease-in-out group-hover:translate-x-[250%] group-hover:translate-y-[200%] group-hover:rotate-[10deg]"></div>
-          <div class="absolute h-full w-full rotate-2 rounded-lg bg-gray-800 transition-transform delay-75 duration-300 ease-in-out group-hover:-translate-x-[25%] group-hover:translate-y-[8%] group-hover:-rotate-[8deg]"></div>
-          <div class="absolute h-1/2 w-1/2 translate-x-1/2 translate-y-1/2 rounded-lg bg-gray-300 transition-transform delay-75 duration-300 ease-in-out group-hover:-translate-x-[150%] group-hover:translate-y-[180%] group-hover:-rotate-[10deg]"></div>
-          <div class="absolute h-full w-full -translate-x-1 -rotate-3 rounded-lg bg-gray-700 transition-transform delay-75 duration-300 ease-in-out group-hover:translate-x-[25%] group-hover:translate-y-[8%] group-hover:rotate-[8deg]"></div>
-          <div class="absolute h-1/2 w-1/2 translate-x-1/2 translate-y-1/2 rounded-lg bg-gray-400 transition-transform delay-75 duration-300 ease-in-out group-hover:-translate-x-[100%] group-hover:-translate-y-[30%] group-hover:rotate-[10deg]"></div>
-          <div class="absolute h-full w-full translate-x-3 translate-y-1 rotate-6 rounded-lg bg-gray-600 transition-transform delay-75 duration-300 ease-in-out group-hover:translate-x-[75%] group-hover:translate-y-[16%] group-hover:rotate-[24deg]"></div>
+          <img src="${this.img8}" class="absolute h-1/2 w-1/2 translate-x-1/2 translate-y-1/2 rounded-lg bg-gray-100 transition-transform delay-75 duration-300 ease-in-out group-hover:-translate-y-[65%] group-hover:translate-x-[200%] group-hover:-rotate-[15deg] object-cover" />
+          <img src="${this.img7}" class="absolute h-full w-full -translate-x-4 -rotate-1 rounded-lg bg-gray-900 transition-transform delay-75 duration-300 ease-in-out group-hover:-translate-x-[75%] group-hover:translate-y-[16%] group-hover:-rotate-[24deg] object-cover" />
+          <img src="${this.img6}" class="absolute h-1/2 w-1/2 translate-x-1/2 translate-y-1/2 rounded-lg bg-gray-200 transition-transform delay-75 duration-300 ease-in-out group-hover:translate-x-[250%] group-hover:translate-y-[200%] group-hover:rotate-[10deg] object-cover" />
+          <img src="${this.img5}" class="absolute h-full w-full rotate-2 rounded-lg bg-gray-800 transition-transform delay-75 duration-300 ease-in-out group-hover:-translate-x-[25%] group-hover:translate-y-[8%] group-hover:-rotate-[8deg] object-cover" />
+          <img src="${this.img4}" class="absolute h-1/2 w-1/2 translate-x-1/2 translate-y-1/2 rounded-lg bg-gray-300 transition-transform delay-75 duration-300 ease-in-out group-hover:-translate-x-[150%] group-hover:translate-y-[180%] group-hover:-rotate-[10deg] object-cover"/>
+          <img src="${this.img3}" class="absolute h-full w-full -translate-x-1 -rotate-3 rounded-lg bg-gray-700 transition-transform delay-75 duration-300 ease-in-out group-hover:translate-x-[25%] group-hover:translate-y-[8%] group-hover:rotate-[8deg] object-cover" />
+          <img src="${this.img2}" class="absolute h-1/2 w-1/2 translate-x-1/2 translate-y-1/2 rounded-lg bg-gray-400 transition-transform delay-75 duration-300 ease-in-out group-hover:-translate-x-[100%] group-hover:-translate-y-[30%] group-hover:rotate-[10deg] object-cover" />
+          <img src="${this.img1}" class="absolute h-full w-full translate-x-3 translate-y-1 rotate-6 rounded-lg  bg-gray-600 transition-transform delay-75 duration-300 ease-in-out group-hover:translate-x-[75%] group-hover:translate-y-[16%] group-hover:rotate-[24deg] object-cover" />
         </div>`;
   }
 }
